28c0889fa2bdaeb2a0b6502fe179daae9ed28c38
Hajira Marwah <hajiranadia12@gmail.com>
2025-10-14 19:41:44 +0530

    color-changer

--- 1-colorChanger/colorChange.js
+++ 1-colorChanger/colorChange.js
@@ -5,17 +5,29 @@ button.forEach(function (button) {
   button.addEventListener("click", function (e) {
     console.log(e);
     console.log(e.target.id);
-    if (e.target.id === "grey") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "white") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "blue") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "green") {
-      body.style.backgroundColor = e.target.id;
+    // if (e.target.id === "grey") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === "white") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === "blue") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+    // if (e.target.id === "green") {
+    //   body.style.backgroundColor = e.target.id;
+    // }
+
+    // switch
+    switch(e.target.id){
+        case "grey":
+        case "white":
+        case "blue":
+        case "green":
+         return body.style.backgroundColor=e.target.id
+         break
+        default:
+            console.log("no color matchess")
     }
   });
 });
